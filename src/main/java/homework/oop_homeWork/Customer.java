3cd3f6254e87998c1e3061b1ce9de8e779069fd1
sBullis <104388303+sBullis@users.noreply.github.com>
2022-06-21 18:00:12 +0300

    md

--- src/main/java/homework/oop_homeWork/Customer.java
+++ src/main/java/homework/oop_homeWork/Customer.java
@@ -1,4 +1,29 @@
 package homework.oop_homeWork;
 
 public class Customer {
+    String name;
+    boolean member = false;
+    String memberType;
+
+    public Customer(String name, boolean member, String memberType) {
+        this.name = name;
+        this.member = member;
+        this.memberType = memberType;
+    }
+
+    public boolean isMember() {
+        return member;
+    }
+
+    public void setMember(boolean member) {
+        this.member = member;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType;
+    }
 }
---
+++ src/main/java/homework/oop_homeWork/Main.java
@@ -0,0 +1,15 @@
+package homework.oop_homeWork;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+        System.out.println(Customer.toString());
+
+    }
+}
